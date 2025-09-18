38a09634e37448fa6f8881449ec0b5124942774a
jorge-lobo <jorge-lobo@outlook.pt>
2025-09-18 23:19:59 +0100

    refactor(theme): separate light and dark color schemes and composition locals

---
+++ app/src/main/java/com/jorgelobo/koobe/ui/theme/CompositionLocals.kt
@@ -0,0 +1,11 @@
+package com.jorgelobo.koobe.ui.theme
+
+import androidx.compose.runtime.staticCompositionLocalOf
+
+val LocalAppColors = staticCompositionLocalOf<AppColorScheme> {
+    error("No AppColorScheme provided - wrap your composable in KoobeTheme")
+}
+
+val LocalAppTypography = staticCompositionLocalOf<AppTypography> {
+    error("No AppTypography provided - wrap your composable in KoobeTheme")
+}
