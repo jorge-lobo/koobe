1c239dc8cf475f748db7b144714a37129a44f933
jorge-lobo <jorge-lobo@outlook.pt>
2026-05-06 17:59:30 +0100

    feat(ui): support error messages in AppInputText component
    Extend `InputFieldConfig` with `errorMessage`.
    Update `AppInputText` to display contextual error messages when in error state instead of generic placeholder.

--- app/src/main/java/com/jorgelobo/koobe/ui/components/base/inputs/fields/InputFieldConfig.kt
+++ app/src/main/java/com/jorgelobo/koobe/ui/components/base/inputs/fields/InputFieldConfig.kt
@@ -7,6 +7,7 @@ data class InputFieldConfig(
     val value: String,
     val label: String,
     val placeholder: String? = null,
+    val errorMessage: String? = null,
     val icon: IconPack? = null,
     val isPassword: Boolean = false,
     val state: InputState = InputState.DEFAULT,
